f1e822f980dd73ae178cf689d0267cd888a4ad92
ancs214 <ancs214@gmail.com>
2022-03-27 13:05:04 -0400

    initial commit

---
+++ app.js
@@ -0,0 +1,29 @@
+//make a var to hold process.argv array minus the execPath and JS file path -- isolating command-line arguments only
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+console.log(profileDataArgs);
+
+// const printProfileData = profileDataArr => {
+//     for (let i=0; i<profileDataArr.length; i++) {
+//         console.log(profileDataArr[i]);
+//     }
+// };
+
+// printProfileData(profileDataArgs);
+
+const printProfileData = profileDataArr => {
+    // This...
+    for (let i = 0; i < profileDataArr.length; i += 1) {
+      console.log(profileDataArr[i]);
+    }
+  
+    console.log('================');
+  
+    // Is the same as this...
+    // profileDataArr.forEach((profileItem) => {
+    //   console.log(randomWord)  });
+
+    //also the same as this...
+    profileDataArr.forEach((profileItem => console.log(profileItem)));
+   };
+
+  printProfileData(profileDataArgs);
