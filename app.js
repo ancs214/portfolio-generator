ec29cb887224ac052d7b1498f31d0f87dcc33832
ancs214 <ancs214@gmail.com>
2022-03-29 18:23:38 -0400

    creating html template to plug in template literals

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 //make a var to hold process.argv array minus the execPath and JS file path -- isolating command-line arguments only
-const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+// const profileDataArgs = process.argv.slice(2, process.argv.length);
+// console.log(profileDataArgs);
 
 // const printProfileData = profileDataArr => {
 //     for (let i=0; i<profileDataArr.length; i++) {
@@ -28,5 +28,34 @@ console.log(profileDataArgs);
 
 //   printProfileData(profileDataArgs);
 
-const generatePage = () => 'Name: Jane, Github: janehub';
 
+const profileDataArgs = process.argv.slice(2, process.argv.length);
+// const name = profileDataArgs[0];
+// const github = profileDataArgs[1];
+//above is same as:
+const [name, github] = profileDataArgs;
+
+
+
+const generatePage = (userName, githubName) => {
+    return `
+  <!DOCTYPE html> 
+  <html lang="en"> 
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portfolio Demo</title>
+  </head>
+
+  <body>
+    <h1>${name}</h1>
+    <h2><a href="https://github.com/${github}">Github</a></h2>
+  </body>
+  </html>
+  `;
+};
+
+
+  console.log(name, github);
+  console.log(generatePage(name, github));
