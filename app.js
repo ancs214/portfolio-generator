d8000f3ee6d09e9ea962245d9e1b8b75a78de7b4
ancs214 <ancs214@gmail.com>
2022-04-01 13:37:23 -0400

    added ability to prompt user to add multiple projects

--- app.js
+++ app.js
@@ -16,9 +16,9 @@
 //     for (let i = 0; i < profileDataArr.length; i += 1) {
 //       console.log(profileDataArr[i]);
 //     }
-  
+
 //     console.log('================');
-  
+
 //     // Is the same as this...
 //     // profileDataArr.forEach((profileItem) => {
 //     //   console.log(randomWord)  });
@@ -60,15 +60,93 @@ const inquirer = require('inquirer');
 //   console.log('Portfolio complete! Check out index.html to see the output!');
 // });
 
-  //now when you make a command line argument (i.e. node app jane janehub), the result shows in the html template
+//now when you make a command line argument (i.e. node app jane janehub), the result shows in the html template
 
 
-  inquirer
-  .prompt([
-    {
-      type: 'input',
-      name: 'name',
-      message: 'What is your name?'
+const promptUser = () => {
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is your name?'
+        },
+        {
+            type: 'input',
+            name: 'github',
+            message: 'Enter your GitHub Username'
+        },
+        {
+            type: 'input',
+            name: 'about',
+            message: 'Provide some information about yourself:'
+        }
+    ]);
+};
+
+
+
+//pass in an argument portfolioData so we can add project data to the argument variable and then call the function with the modified data
+const promptProject = portfolioData => {
+    // If there's no 'projects' array property, create an empty projects array
+    if (!portfolioData.projects) {
+        portfolioData.projects = [];
     }
-  ])
-  .then(answers => console.log(answers));
+    console.log(`
+  =================
+  Add a New Project
+  =================
+  `);
+    return inquirer.prompt([
+        {
+            type: 'input',
+            name: 'name',
+            message: 'What is the name of your project?'
+        },
+        {
+            type: 'input',
+            name: 'description',
+            message: 'Provide a description of the project (Required)'
+        },
+        {
+            type: 'checkbox',
+            name: 'languages',
+            message: 'What did you build this project with? (Check all that apply)',
+            choices: ['JavaScript', 'HTML', 'CSS', 'ES6', 'jQuery', 'Bootstrap', 'Node']
+        },
+        {
+            type: 'input',
+            name: 'link',
+            message: 'Enter the GitHub link to your project. (Required)'
+        },
+        {
+            type: 'confirm',
+            name: 'feature',
+            message: 'Would you like to feature this project?',
+            default: false
+        },
+        {
+            type: 'confirm',
+            name: 'confirmAddProject',
+            message: 'Would you like to enter another project?',
+            default: false
+        }
+    ])
+    .then(projectData => {
+        //push project data to array
+        portfolioData.projects.push(projectData);
+        //if user selects yes when asked if they want to add another project, then run the function again for their next project,
+        //else return portfolio data -- this data will be connected to the promptProject function since it is passed in as an argument
+        if (projectData.confirmAddProject) {
+            return promptProject(portfolioData);
+          } else {
+            return portfolioData;
+          }
+        })
+};
+
+//since these are promises, it is asynchronous and will need to use the .then statement for order of events
+promptUser()
+.then(promptProject)
+.then(portfolioData => {
+  console.log(portfolioData);
+});
