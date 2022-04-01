3c3ab748bf87d98d2f9ff6d21979c297af67e4d9
ancs214 <ancs214@gmail.com>
2022-04-01 12:56:59 -0400

    add inquirer package and begin inquirer prompt

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//**************NOTES******************/
 //make a var to hold process.argv array minus the execPath and JS file path -- isolating command-line arguments only
 // const profileDataArgs = process.argv.slice(2, process.argv.length);
 // console.log(profileDataArgs);
@@ -32,22 +33,42 @@
 //using 'require statement', which is a built-in function that is globally available in node.js
 //it allows app.js to access the 'fs module' functions
 //modules: any reusable piece of code from the core library or even a separate JS file
+//**************NOTES******************/
 
 
+const inquirer = require('inquirer');
+//console.log(inquirer) to verify inquirer has been successfully imported into script
+// const fs = require('fs');
+// const generatePage = require('./src/page-template.js');
 
-const fs = require('fs');
-const generatePage = require('./src/page-template.js');
 
-const profileDataArgs = process.argv.slice(2);
-// const name = profileDataArgs[0];
-// const github = profileDataArgs[1];
-//above is same as:
-const [name, github] = profileDataArgs;
+//******************NOTES************************/
+//got rid of process.arv stuff since we are now getting our info from inquiry module
+// const profileDataArgs = process.argv.slice(2);
+// // const name = profileDataArgs[0];
+// // const github = profileDataArgs[1];
+// //above is same as:
+// const [name, github] = profileDataArgs;
+//*****************************************/
 
-fs.writeFile('./index.html', generatePage(name, github), err => {
-  if (err) throw new Error(err);
 
-  console.log('Portfolio complete! Check out index.html to see the output!');
-});
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//   if (err) throw err;
+
+//   console.log('Portfolio complete! Check out index.html to see the output!');
+// });
 
   //now when you make a command line argument (i.e. node app jane janehub), the result shows in the html template
+
+
+  inquirer
+  .prompt([
+    {
+      type: 'input',
+      name: 'name',
+      message: 'What is your name?'
+    }
+  ])
+  .then(answers => console.log(answers));
